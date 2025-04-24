5eaa157e20a76cc706b71fb4569bf4c4318cc105
webgliders <haleemcoco14@gmail.com>
2025-04-24 12:57:11 +0100

    fixed  errors

--- BeamAfrique/urls.py
+++ BeamAfrique/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/', include('analytics.urls')),
+    # path('api/', include('analytics.urls')),mm5
 ]
 
 if settings.DEBUG:
--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,17 @@ from rest_framework import serializers
 from .models import ContentUpload, MagazineContent
 
 class ContentUploadSerializer(serializers.ModelSerializer):
-    file = serializers.SerializerMethodField()
+    # file = serializers.SerializerMethodField()
+    file = serializers.FileField(write_only=True)
+    # …but expose it back as a URL in “file_url”
+    file_url = serializers.SerializerMethodField(read_only=True)
+    
 
     class Meta:
         model = ContentUpload
         fields = '__all__'
 
-    def get_file(self, obj):
+    def get_file_url(self, obj):
         request = self.context.get('request')
         if obj.file and request:
             return request.build_absolute_uri(obj.file.url)
@@ -16,15 +20,25 @@ class ContentUploadSerializer(serializers.ModelSerializer):
 
 
 class MagazineContentSerializer(serializers.ModelSerializer):
-    cover_image = serializers.SerializerMethodField()
+    cover_image = serializers.ImageField(write_only=True)
+
+    cover_image_url = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = MagazineContent
         fields = '__all__'
 
-    def get_cover_image(self, obj):
-        request = self.context.get('request')
-        if obj.cover_image and request:
-            return request.build_absolute_uri(obj.cover_image.url)
-        return None
+
+    def get_cover_image_url(self, obj):
+            """Build an absolute URL to the image for read operations."""
+            request = self.context.get('request')
+            if obj.cover_image and request:
+                return request.build_absolute_uri(obj.cover_image.url)
+            return None
+        
+    # def get_cover_image(self, obj):
+    #     request = self.context.get('request')
+    #     if obj.cover_image and request:
+    #         return request.build_absolute_uri(obj.cover_image.url)
+    #     return None
 
