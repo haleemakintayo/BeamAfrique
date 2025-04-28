f07d09ca929e3b9aa2dff34ad8c1ef322862fff9
webgliders <haleemcoco14@gmail.com>
2025-04-28 19:54:09 +0100

    added deploy settings

--- BeamAfrique/settings.py
+++ BeamAfrique/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-__8eki3)j&n1-0b6i^3@v@8x+3d%oody$d5tum*#)7w-eyp362'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['api.beamafrique.com']
 
 
 # Application definition
@@ -56,7 +56,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'BeamAfrique.urls'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+  'https://beamafrique.com',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -143,7 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
+MEDIA_URL  = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
